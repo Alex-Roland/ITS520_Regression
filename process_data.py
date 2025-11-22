b41b90507a3eab3d72acfb064d629a415f6d4661
Alex Roland <rolanda@purdue.edu>
2025-11-21 20:04:16 -0600

    Updated process_data.py

--- process_data.py
+++ process_data.py
@@ -1,8 +1,6 @@
 import pandas as pd
 
-working_dir = "Regression"
-
-inside_data = pd.read_csv(f"{working_dir}/garage_temp_history.csv")
+inside_data = pd.read_csv("garage_temp_history.csv")
 inside_data['date'] = pd.to_datetime(inside_data['date']).dt.date
 data_temp = inside_data[inside_data['entity_id'] == 'sensor.garage_temp_and_humidity_air_temperature']
 daily_avg_temp = data_temp.groupby(data_temp['date'], as_index=False)['state'].mean().round(1)
@@ -11,9 +9,9 @@ daily_avg_humidity = data_humidity.groupby(data_humidity['date'], as_index=False
 daily_avg_merged = pd.merge(daily_avg_temp, daily_avg_humidity, on='date', how='inner', suffixes=('_temp', '_humidity'))
 daily_avg_merged = daily_avg_merged.rename(columns={'state_temp': 'inside_temp', 'state_humidity': 'inside_humidity'})
 
-outside_data = pd.read_csv(f"{working_dir}/outside_temp_history.csv")
+outside_data = pd.read_csv("outside_temp_history.csv")
 outside_data['date'] = pd.to_datetime(outside_data['date']).dt.date
 
 all_data_merged = pd.merge(outside_data, daily_avg_merged, on='date', how='inner')
 all_data_merged = all_data_merged.drop('date', axis=1)
-all_data_merged.to_csv(f"{working_dir}/all_data_merged.csv", index=False)
+all_data_merged.to_csv("all_data_merged.csv", index=False)
